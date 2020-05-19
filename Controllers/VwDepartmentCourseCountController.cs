ce81864a0627653fc73394b4b6e3d836e2057722
jccbrian <65386787+jccbrian@users.noreply.github.com>
2020-05-19 14:30:36 +0800

    加入專案檔案。

---
+++ Controllers/VwDepartmentCourseCountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VwDepartmentCourseCountController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+
+        public VwDepartmentCourseCountController(ContosouniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VwCourseStudentCount
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VwDepartmentCourseCount>>> GetVwDepartmentCourse()
+        {
+            return await _context.VwDepartmentCourseCount.FromSqlRaw("Select * From VwDepartmentCourseCount").ToListAsync();
+            //return await _context.VwDepartmentCourseCount.ToListAsync();
+        }
+    }
+}
---
+++ Migrations/20200519055329_DateModified.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class DateModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "Person",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "Department",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "Course",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "Person",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "Department",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateModified",
+                table: "Course",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
